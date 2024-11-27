6df7eb41b14ecf1c16c9135d93d01b177a179ed5
Hudson Johnson <hudsonjohnson.visualmedia@gmail.com>
2024-11-27 15:44:40 +0000

    Header

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
     darkMode: ['class'],
     content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
     	extend: {
+			fontFamily: {
+				'sans': ['"Montserrat"'],
+			},
     		borderRadius: {
     			lg: 'var(--radius)',
     			md: 'calc(var(--radius) - 2px)',
@@ -53,5 +57,5 @@ export default {
     		}
     	}
     },
-	plugins: [require("tailwindcss-animate")],
+	plugins: [require("tailwindcss-animate"),require('daisyui'),],
 }
